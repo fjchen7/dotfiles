6501ff24bd182ecce8e8c8f706e6b3966d6d3894
fjchen7 <fjchen7@gmail.com>
2020-08-29 21:17:52 +0800

    refactor(common): refactor autojump and diff-so-fancy installations

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ ! -e ${HOME}/.local/diff-so-fancy ]]; then
-    [ ! -e ${HOME}/.local ] && mkdir ${HOME}/.local
+if [[ -z "$(command -v diff-so-fancy)" ]]; then
     git clone https://github.com/so-fancy/diff-so-fancy ${HOME}/.local/diff-so-fancy
+    ln -s ${HOME}/.local/diff-so-fancy/bin/diff-so-fancy /usr/local/bin/diff-so-fancy
 fi
--- zsh/path.zshrc
+++ zsh/path.zshrc
@@ -1,3 +1,2 @@
 export PATH=~/.local/bin:$PATH
-export PATH=~/.local/diff-so-fancy:$PATH
 export PATH=${DOTFILES_ROOT}/bin:$PATH
