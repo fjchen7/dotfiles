d7f4816049aa149c607f01f429c921e3e8387679
fjchen7 <fjchen7@gmail.com>
2022-03-25 11:07:54 +0800

    Update karabiner configuration (Goku)

--- karabiner/scripts/reload_config.sh
+++ karabiner/scripts/reload_config.sh
@@ -7,10 +7,16 @@ hs -c "hs.reload()"
 
 # reload goku
 sleep 1
-msg="$(goku 2>&1)"
+msg="$(/opt/homebrew/bin/goku 2>&1)"
 if [ "$msg" != "Done!" ]; then
-    alert "⚠️Fail to Reload Goku!"
-    exit
+    alert "⚠️Fail to reload Goku: $msg"
 else
-    alert "Goku Config Reloaded"
+    alert "Goku configuratioin reloaded"
+fi
+
+/opt/homebrew/bin/brew services restart yabai > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+    alert "⚠️Fail to restart yabai"
+else
+    alert "Yabai restarted"
 fi
