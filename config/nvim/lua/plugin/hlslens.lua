01ca6e3cc6648ba13e12b19993c82bcc6ecaca28
fjchen7 <fjchen7@gmail.com>
2022-12-27 22:21:23 +0800

    nvim: dismiss virtual text in hlslens

--- config/nvim/lua/plugin/hlslens.lua
+++ config/nvim/lua/plugin/hlslens.lua
@@ -1,34 +1,9 @@
 -- https://github.com/petertriho/nvim-scrollbar#setup-packer
 -- setup up and overwrite hlslens
 require("scrollbar.handlers.search").setup({
-  -- https://github.com/kevinhwang91/nvim-hlslens#customize-virtual-text
-  override_lens = function(render, posList, nearest, idx, relIdx)
-    local sfw = vim.v.searchforward == 1
-    local indicator, text, chunks
-    local absRelIdx = math.abs(relIdx)
-    if absRelIdx > 1 then
-      indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and '▲' or '▼')
-    elseif absRelIdx == 1 then
-      indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
-    else
-      indicator = ''
-    end
-
-    local lnum, col = unpack(posList[idx])
-    if nearest then
-      local cnt = #posList
-      if indicator ~= '' then
-        text = ('[%s %d/%d]'):format(indicator, idx, cnt)
-      else
-        text = ('[%d/%d]'):format(idx, cnt)
-      end
-      chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLensNear' } }
-    else
-      text = ('[%s %d]'):format(indicator, idx)
-      chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLens' } }
-    end
-    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
-  end,
+  -- virtual text (https://github.com/kevinhwang91/nvim-hlslens#customize-virtual-text) is
+  -- distracting so I dismiss all.
+  override_lens = function() end
 })
 
 -- https://github.com/kevinhwang91/nvim-hlslens#minimal-configuration
