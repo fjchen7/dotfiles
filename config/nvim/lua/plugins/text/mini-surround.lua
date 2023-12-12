c0ca7e16efc3de0ad3192ab84769e65160e5450d
fjchen7 <fjchen7@gmail.com>
2023-12-12 15:51:37 +0800

    nvim: support k(<>) pair in mini-surround

--- config/nvim/lua/plugins/text/mini-surround.lua
+++ config/nvim/lua/plugins/text/mini-surround.lua
@@ -5,15 +5,23 @@ return {
   -- enabled = false,
   opts = {
     mappings = {
-      add = "sa",            -- Add surrounding in Normal and Visual modes
-      delete = "sd",         -- Delete surrounding
-      replace = "sr",        -- Replace surrounding
+      add = "sa", -- Add surrounding in Normal and Visual modes
+      delete = "sd", -- Delete surrounding
+      replace = "sr", -- Replace surrounding
       update_n_lines = "sn", -- Update `n_lines`
-      find = "",             -- Find surrounding (to the right)
-      find_left = "",        -- Find surrounding (to the left)
-      highlight = "",        -- Highlight surrounding
-      suffix_last = "",      -- Suffix to search with "prev" method
-      suffix_next = "",      -- Suffix to search with "next" method
+      find = "", -- Find surrounding (to the right)
+      find_left = "", -- Find surrounding (to the left)
+      highlight = "", -- Highlight surrounding
+      suffix_last = "", -- Suffix to search with "prev" method
+      suffix_next = "", -- Suffix to search with "next" method
     },
-  }
+    custom_surroundings = {
+      ["k"] = {
+        -- input = { left = "<", right = ">" },
+        -- input = { "%b()", "^.().*().$" },
+        input = { { "%b<>" }, "^.().*().$" },
+        output = { left = "<", right = ">" },
+      },
+    },
+  },
 }
