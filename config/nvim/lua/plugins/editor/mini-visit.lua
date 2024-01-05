3611d5da8bcca73600d6148a9007ec8c1207f00f
fjchen7 <fjchen7@gmail.com>
2024-01-06 00:14:31 +0800

    Remove keymap of labels in mini-visit

--- config/nvim/lua/plugins/editor/mini-visit.lua
+++ config/nvim/lua/plugins/editor/mini-visit.lua
@@ -30,72 +30,72 @@ return {
       end,
       desc = "remove all visited files (MiniVisits)",
     },
-    -- Label is the secondary category
-    {
-      "<leader>mL",
-      function() MiniVisits.select_label("", "") end,
-      desc = "list files in label (MiniVisits)",
-    },
-    {
-      "<leader>ma",
-      function()
-        local labels = MiniVisits.list_labels("", "")
-        labels = { "[New]", table.unpack(labels) }
-        if #labels ~= 0 then
-          vim.ui.select(labels, {
-            prompt = "Select a label to add",
-          }, function(choice)
-            if choice then
-              if choice == "[New]" then
-                MiniVisits.add_label()
-              else
-                MiniVisits.add_label(choice)
-              end
-            end
-          end)
-        end
-      end,
-      desc = "add file to label (MiniVisits)",
-    },
-    {
-      "<leader>m<S-BS>",
-      function()
-        local labels = MiniVisits.list_labels("", "")
-        if #labels ~= 0 then
-          vim.ui.select(labels, {
-            prompt = "Select a label to remove",
-          }, function(label)
-            if label then
-              MiniVisits.remove_label(label, "")
-              vim.notify("Remove label " .. label)
-            end
-          end)
-        else
-          require("lazy.core.util").warn("No label to delete", { title = "Mini.Visit" })
-        end
-      end,
-      desc = "remove label (MiniVisits)",
-    },
-    {
-      "<leader>m<BS>",
-      function()
-        local labels = MiniVisits.list_labels()
-        if #labels ~= 0 then
-          vim.ui.select(labels, {
-            prompt = "Remove file from label",
-          }, function(label)
-            local path = vim.fn.expand("%:p:.")
-            if label then
-              MiniVisits.remove_label(label, path)
-              vim.notify("Remove current file from label " .. label)
-            end
-          end)
-        else
-          require("lazy.core.util").warn("No label to delete", { title = "Mini.Visit" })
-        end
-      end,
-      desc = "remove current file from label (MiniVisits)",
-    },
+    -- -- Label is the secondary category
+    -- {
+    --   "<leader>mL",
+    --   function() MiniVisits.select_label("", "") end,
+    --   desc = "list files in label (MiniVisits)",
+    -- },
+    -- {
+    --   "<leader>ma",
+    --   function()
+    --     local labels = MiniVisits.list_labels("", "")
+    --     labels = { "[New]", table.unpack(labels) }
+    --     if #labels ~= 0 then
+    --       vim.ui.select(labels, {
+    --         prompt = "Select a label to add",
+    --       }, function(choice)
+    --         if choice then
+    --           if choice == "[New]" then
+    --             MiniVisits.add_label()
+    --           else
+    --             MiniVisits.add_label(choice)
+    --           end
+    --         end
+    --       end)
+    --     end
+    --   end,
+    --   desc = "add file to label (MiniVisits)",
+    -- },
+    -- {
+    --   "<leader>m<S-BS>",
+    --   function()
+    --     local labels = MiniVisits.list_labels("", "")
+    --     if #labels ~= 0 then
+    --       vim.ui.select(labels, {
+    --         prompt = "Select a label to remove",
+    --       }, function(label)
+    --         if label then
+    --           MiniVisits.remove_label(label, "", "")
+    --           vim.notify("Remove label " .. label)
+    --         end
+    --       end)
+    --     else
+    --       require("lazy.core.util").warn("No label to delete", { title = "Mini.Visit" })
+    --     end
+    --   end,
+    --   desc = "remove label (MiniVisits)",
+    -- },
+    -- {
+    --   "<leader>m<BS>",
+    --   function()
+    --     local labels = MiniVisits.list_labels()
+    --     if #labels ~= 0 then
+    --       vim.ui.select(labels, {
+    --         prompt = "Remove file from label",
+    --       }, function(label)
+    --         local path = vim.fn.expand("%:p:.")
+    --         if label then
+    --           MiniVisits.remove_label(label, path)
+    --           vim.notify("Remove current file from label " .. label)
+    --         end
+    --       end)
+    --     else
+    --       require("lazy.core.util").warn("No label to delete", { title = "Mini.Visit" })
+    --     end
+    --   end,
+    --   desc = "remove current file from label (MiniVisits)",
+    -- },
   },
   opts = {},
 }
