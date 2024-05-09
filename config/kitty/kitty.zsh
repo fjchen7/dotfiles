cbc8b1997f5331ee26e19deec189ad0f60fd2931
fjchen7 <fjchen7@gmail.com>
2024-05-09 23:52:52 +0800

    Change kitty font size

--- config/kitty/kitty.zsh
+++ config/kitty/kitty.zsh
@@ -4,7 +4,7 @@ alias icat="kitty +kitten icat --align left"
 # https://sw.kovidgoyal.net/kitty/kittens/remote_file/
 alias ssh="kitty +kitten ssh"
 # https://sw.kovidgoyal.net/kitty/kittens/diff/
-alias d="kitty +kitten diff"
+alias diff="kitty +kitten diff"
 # https://sw.kovidgoyal.net/kitty/kittens/hyperlinked_grep/
 alias hg="kitty +kitten hyperlinked_grep"
-alias theme="kitty +kitten themes"
+alias themes="kitty +kitten themes"
