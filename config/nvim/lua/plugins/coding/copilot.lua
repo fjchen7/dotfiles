92c60dcfb59f9624ccf2b072cac16fe5132a78ad
fjchen7 <fjchen7@gmail.com>
2025-03-22 20:18:39 +0800

    Update nvim

--- config/nvim/lua/plugins/coding/copilot.lua
+++ config/nvim/lua/plugins/coding/copilot.lua
@@ -8,18 +8,21 @@ return {
     { mode = { "i" }, "<C-y>", "<CMD>Copilot panel<CR>", desc = "[copilot] open panel" },
   },
   opts = {
-    --   suggestion = {
-    --     enabled = true,
-    --     auto_trigger = true,
-    --     keymap = {
-    --       accept = false,
-    --       accept_word = false,
-    --       accept_line = false,
-    --       next = "<M-]>",
-    --       prev = "<M-[>",
-    --       dismiss = false,
-    --     },
-    --   },
+    filetypes = {
+      snacks_picker_input = false,
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = false,
+        accept_word = "<M-Tab>",
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = false,
+      },
+    },
     panel = {
       enabled = true,
       keymap = {
